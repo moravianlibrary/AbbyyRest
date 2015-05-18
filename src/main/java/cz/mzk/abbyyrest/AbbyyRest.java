46cbd918eb91eb6253c1fae826d0503b0b399877
popelka <thomm87gmail.com>
2015-05-18 15:56:42 +0200

    Nestpusti se v Jetty - No injection source found for a parameter of type public java.lang.String cz.mzk.abbyyrest.AbbyyRest.put(java.io.InputStream) at index 0.

--- src/main/java/cz/mzk/abbyyrest/AbbyyRest.java
+++ src/main/java/cz/mzk/abbyyrest/AbbyyRest.java
@@ -1,19 +1,58 @@
 package cz.mzk.abbyyrest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.ws.rs.core.Response;
+import org.apache.commons.io.FileUtils;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 /**
  * Created by rumanekm on 5.5.15.
  */
 @Path("/ocr")
 public class AbbyyRest {
+
+    String path = "C:\\IMGSTR\\IN\\";
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String getList() {
+        return listFiles("C:\\");
     }
+
+    @POST
+    @Path("/upload")
+    @Consumes({"multipart/form-data"})
+    @Produces(MediaType.TEXT_PLAIN)
+    public String put(@FormDataParam("file") InputStream is,
+                      @FormDataParam("file") FormDataContentDisposition info) {
+        File pictureIn = new File(path + info.getFileName());
+        try {
+            FileUtils.copyInputStreamToFile(is, pictureIn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "neni tam";
+        }
+        return "je tam";
+    }
+
+
+
+private String listFiles(String path){
+
+    File folder = new File(path);
+    File[] listOfFiles = folder.listFiles();
+    String result="";
+
+    for (File listOfFile : listOfFiles) {
+        result = result + "\n" + listOfFile.getName();
+    }
+
+    return result;
+}
 
 }

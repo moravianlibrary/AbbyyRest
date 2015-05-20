02f748384ec471647b0472d6f2f7e38a04fe9411
popelka <thomm87gmail.com>
2015-05-20 08:15:13 +0200

    POST zpracovan. Cesta do vstupního adresáře se nastaví přes globální proměnnou ABBYY_IN (v síti NDK \\10.10.2.21\WORK01). Syntaxe: /ocr/jpx/uuid kde jpx je přípona souboru (akceptují se jpg nebo jp2) a uuid musi mít validní tvar.

--- src/main/java/cz/mzk/abbyyrest/AbbyyRest.java
+++ src/main/java/cz/mzk/abbyyrest/AbbyyRest.java
@@ -1,43 +1,51 @@
 package cz.mzk.abbyyrest;
 
-
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import javax.ws.rs.core.Response.*;
 import javax.ws.rs.core.Response;
 import org.apache.commons.io.FileUtils;
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 /**
  * Created by rumanekm on 5.5.15.
  */
 @Path("/ocr")
 public class AbbyyRest {
 
-    String path = "C:\\IMGSTR\\IN\\";
+    String path = System.getenv("ABBYY_IN");
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getList() {
-        return listFiles("C:\\");
+        return listFiles(path);
     }
 
     @POST
-    @Path("/upload")
+    @Path("/{extension}/{uuid}")
     @Consumes({"multipart/form-data"})
     @Produces(MediaType.TEXT_PLAIN)
-    public String put(@FormDataParam("file") InputStream is,
-                      @FormDataParam("file") FormDataContentDisposition info) {
-        File pictureIn = new File(path + info.getFileName());
+    public Response put(@PathParam("uuid") String uuid, @PathParam("extension") String extension, InputStream is)  {
+        if(!(extension.equalsIgnoreCase("jpg")||extension.equalsIgnoreCase("jp2"))){
+            return Response.status(Status.BAD_REQUEST).entity("Deklarovan chybny format " + extension + " Je akceprovan jpg nabo jp2.").type("text/plain").build();
+        }
+        if(!(uuid.matches("[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"))){
+            return Response.status(Status.BAD_REQUEST).entity("Neplatne UUID\"" + uuid + "\".").type("text/plain").build();
+        }
+        File f = new File(path + uuid + "." + extension);
         try {
-            FileUtils.copyInputStreamToFile(is, pictureIn);
+            FileUtils.copyInputStreamToFile(is, f);
         } catch (IOException e) {
             e.printStackTrace();
-            return "neni tam";
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Zapis souboru selhal.").type("text/plain").build();
         }
-        return "je tam";
+        return Response.status(Status.OK).entity("Pozadavek OCR zarazen do fronty.").type("text/plain").build();
     }
 
 
@@ -48,8 +56,8 @@ private String listFiles(String path){
     File[] listOfFiles = folder.listFiles();
     String result="";
 
-    for (File listOfFile : listOfFiles) {
-        result = result + "\n" + listOfFile.getName();
+    for (int i = 0; i < listOfFiles.length; i++) {
+          result = result + "\n" + listOfFiles[i].getName();
     }
 
     return result;
